6e343f8ca4cf0ad3fc9de73522d8951ca84a03bf
wfhopkins <will.hop@gmail.com>
2023-07-26 21:13:51 -0700

    initial commit. an attempt has been made

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,23 @@
 class Store < ActiveRecord::Base
+  
+  store = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+  )
 
+  store = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+  )
+
+  store = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+  )
 end
